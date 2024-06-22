e50cb46e0c57d66df5b3df4de2a7f9e66f2ac91d
sbdarjun <arzoonsubedi123@gmail.com>
2024-06-22 16:41:20 -0500

    added get candidate details and update details by id api endpoint

--- jobtracking_api/src/main/java/com/arsbd/jobtracking/controller/CandidateController.java
+++ jobtracking_api/src/main/java/com/arsbd/jobtracking/controller/CandidateController.java
@@ -1,8 +1,10 @@
 package com.arsbd.jobtracking.controller;
 
+import com.arsbd.jobtracking.exception.ResourceNotFoundException;
 import com.arsbd.jobtracking.model.Candidate;
 import com.arsbd.jobtracking.repository.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,14 +19,37 @@ public class CandidateController {
 
     // get all Candidate
     @GetMapping("/candidates")
-    public List<Candidate> getAllCandidate(){
+    public List<Candidate> getAllCandidate() {
         return candidateRepository.findAll();
     }
 
     // create new job Candidate rest api
     @PostMapping("/candidates")
-    public Candidate createCandidate(@RequestBody Candidate candidate){
+    public Candidate createCandidate(@RequestBody Candidate candidate) {
         return candidateRepository.save(candidate);
     }
 
+    // get candidate by id
+    @GetMapping("/candidates/{id}")
+    public ResponseEntity<Candidate> getCandidateById(@PathVariable Long id) {
+        Candidate candidate = candidateRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Candidate not exist with id :" + id));
+        return ResponseEntity.ok(candidate);
+    }
+
+    // update candidate by Id
+    @PutMapping("/candidates/{id}")
+    public ResponseEntity<Candidate> updateCandidate(@PathVariable Long id, @RequestBody Candidate candidateDetails){
+        Candidate candidate = candidateRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Candidate not exist with id :" + id));
+        candidate.setFullName(candidateDetails.getFullName());
+        candidate.setCompanyName(candidateDetails.getCompanyName());
+        candidate.setJobPosition(candidateDetails.getJobPosition());
+        candidate.setWorkAuthorization(candidateDetails.getWorkAuthorization());
+        candidate.setAppliedDate(candidateDetails.getAppliedDate());
+        candidate.setJobDescription(candidateDetails.getJobDescription());
+
+        Candidate updateCandidate= candidateRepository.save(candidate);
+        return ResponseEntity.ok(updateCandidate);
+    }
 }
